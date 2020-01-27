2e902c183311909802e6166d72c6f06143bffbe0
Miley Wright <dishaliekwright@yahoo.com>
2020-01-27 16:05:57 -0500

    linked movies

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import {Route} from 'react-router-dom';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -14,6 +15,8 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <Route exact path= '/' component={MovieList} />
+      <Route exact path= {`/movies/:id`} component={Movie} />
+
     </div>
   );
 };
